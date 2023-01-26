3a96f03f59a4521911c25d2b9d802c68e5d8b6ed
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2023-01-26 16:57:31 +0400

    Add About component and fix bugs header component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "./components/header/Header";
 import Banner from "./components/banner/Banner";
+import About from "./components/about/About";
 
 function App() {
   return (
     <>
       <Header />
       <Banner />
+      <About />
     </>
   );
 }
