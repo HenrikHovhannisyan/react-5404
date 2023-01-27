c265869cdbd0d638d86f3299a2416fd98feadf6d
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2023-01-27 09:47:09 +0400

    Fix bugs

--- src/components/whatWeDo/WhatWeDo.jsx
+++ src/components/whatWeDo/WhatWeDo.jsx
@@ -29,7 +29,7 @@ const WhatWeDo = () => {
   return (
     <section id="whatWeDo">
       <div className="container-fluid max_width">
-        <h2 className="section_title">what we do</h2>
+        <h2 className="section_title mt-5">what we do</h2>
 
         <div className="whatWeDo_container">
           <div className="row">
