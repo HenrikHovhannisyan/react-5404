994366ded36af055e7433671ecff682f0252a131
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2023-02-07 14:15:06 +0400

    Added 404 page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import NoPage from "../pages/404/NoPage";
 import App from "../App";
 
 const RouteList = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="*" element={<NoPage />} />
         <Route path="/" element={<App />} />
       </Routes>
     </BrowserRouter>
